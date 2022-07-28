f13057ba82207ec5915f23f37df059f33b706140
Sergey-SA <69924143+Sergey-SA@users.noreply.github.com>
2022-07-28 08:58:06 +0300

    Update Lesson_1.java
    add comments

--- Lesson_1.java
+++ Lesson_1.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
+/**
+ * Create at 27.07.2022
+ * @autor Sherstnev Sergey
+ *
+ */
+
+
 public class Lesson_1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // input int from console
         float num1 = scanner.nextFloat();
         float num2 = scanner.nextFloat();
 
+        // sum scanner int
         float sum = num1 + num2;
 
+        // print sum
         System.out.printf("%.4f", sum);
     }
 }
